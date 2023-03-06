5e169b4f3b198b5529cbf74fa9680bcc30aa5595
Ravi <rg.opash@gmail.com>
2023-03-06 17:09:18 +0530

    added userRef....but not worked so its in comments.

--- src/Components/AlertBox/AlertBox.js
+++ src/Components/AlertBox/AlertBox.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import style from './AlertBox.module.css'
+import React, { Fragment } from "react";
+import style from "./AlertBox.module.css";
+// import ReactDOM from "react-dom";
 
 const AlertBox = (props) => {
-    let isValid = true;
+  let isValid = true;
 
-    if(props.msg.alertMsg==="" || props.msg.alertMsg===undefined){
-        isValid = true;
-    }
-    else{
-        isValid = false;
-    }
+  if (props.msg.alertMsg === "" || props.msg.alertMsg === undefined) {
+    isValid = true;
+  } else {
+    isValid = false;
+  }
 
-    return(
-          <div className={`${style.main_alert_box} ${!isValid && style.invalid}`} >
-            <div className={style.alert_box_top}>{props.msg.alertHead}</div>
-            <div className={style.alert_box_down}>
-                <p>{props.msg.alertMsg}</p>
-            </div>
-            <div className={style.alert_box_button}>
-                <button onClick={props.onConfirm}>Okay</button>
-            </div>
-          </div>
-    )
-}
+//   const OverLay = (props) => {
+//     return (
+    
+//   )};
+  return (
+   <Fragment>
+    {/* {ReactDOM.createPortal(<OverLay onConfirm={props.onConfirm} />, document.getElementById('overlay'))} */}
+    <div className={`${style.main_alert_box} ${!isValid && style.invalid}`}>
+    <div className={style.alert_box_top}>{props.msg.alertHead}</div>
+    <div className={style.alert_box_down}>
+      <p>{props.msg.alertMsg}</p>
+    </div>
+    <div className={style.alert_box_button}>
+      <button onClick={props.onConfirm}>Okay</button>
+    </div>
+  </div>
+   </Fragment> 
+  );
+};
 
 export default AlertBox;
-
--- src/Components/UserComponents/UserForm.js
+++ src/Components/UserComponents/UserForm.js
@@ -1,66 +1,75 @@
-import React, {useState} from 'react';
-import style from './UserForm.module.css'
+import React, { useState, } from "react";
+import style from "./UserForm.module.css";
 
 const UserForm = (props) => {
+//   const UserName = useRef();
+//   const UserAge = useRef();
 
-    const [UserName, setUserName] = useState("");
-    const [UserAge, setUserAge] = useState("");
 
-    const onChangeUserName = (event) =>{
-        event.preventDefault();
-        setUserName(event.target.value.trim());
-    } 
 
-    const onChangeUserAge = (event) =>{
-        event.preventDefault();
-        setUserAge(event.target.value);
-    } 
+  const [UserName, setUserName] = useState("");
+  const [UserAge, setUserAge] = useState("");
 
-    const onClickBtn = (event) =>{
-        event.preventDefault();
+  const onChangeUserName = (event) => {
+    event.preventDefault();
+    setUserName(event.target.value.trim());
+  };
 
-        const alert = {
-            alertHead : '',
-            alertMsg : ''
-        }
-        if(UserName.length<=0){
-           alert.alertHead = "Invalid Name";
-           alert.alertMsg = "Plese Enter Username... It can't be empty!";
-        //    props.msg(alert);
-        //    return;   
-        }
-        else if(UserAge.length<=0 || +UserAge<1){
-            alert.alertHead = "Invalid Age";
-            alert.alertMsg = "Plese Enter Valid Age... It can't be less than equal to ZERO!";
-            // props.msg(alert);
-            // return;   
-         }
-        const user = {
-            id: Math.random().toString(),
-            name: UserName,
-            age: UserAge, 
-            msg: alert
-        }
-        // console.log(user);
-        props.formData(user);
-        setUserName("")
-        setUserAge("")
+  const onChangeUserAge = (event) => {
+    event.preventDefault();
+    setUserAge(event.target.value);
+  };
+
+  const onClickBtn = (event) => {
+    event.preventDefault();
+    // console.log(UserAge.current.value);
+    const alert = {
+      alertHead: "",
+      alertMsg: "",
+    };
+    if (UserName.length <= 0) {
+      alert.alertHead = "Invalid Name";
+      alert.alertMsg = "Plese Enter Username... It can't be empty!";
+    } else if (UserAge.length <= 0 || +UserAge < 1) {
+      alert.alertHead = "Invalid Age";
+      alert.alertMsg =
+        "Plese Enter Valid Age... It can't be less than equal to ZERO!";
     }
+    const user = {
+      id: Math.random().toString(),
+      name: UserName,
+      age: UserAge,
+      msg: alert,
+    };
+    console.log(user);
+    props.formData(user);
+    setUserName("")
+    setUserAge("")
+  };
 
-    return(
-        <div className={style.main_user_box}>
-          <div className={style.user_input_box}>
-            <label>Username</label>
-            <input type='text' onChange={onChangeUserName} value={UserName} />
-            <label>Age=(Years)</label>
-            <input type='number' onChange={onChangeUserAge} value={UserAge} />
-          </div>
-          <div className={style.user_button_box}>
-            <button onClick={onClickBtn}>Add User</button>
-          </div>
-        </div>
-    )
-}
+  return (
+    <div className={style.main_user_box}>
+      <div className={style.user_input_box}>
+        <label>Username</label>
+        <input 
+        type="text" 
+        onChange={onChangeUserName} 
+        value={UserName} 
+        // ref={UserName}
+        />
+        <label>Age=(Years)</label>
+        <input 
+        type="number" 
+        onChange={onChangeUserAge} 
+        value={UserAge} 
+        // ref={UserAge}
+        />
+      </div>
+      <div className={style.user_button_box}>
+        <button onClick={onClickBtn}>Add User</button>
+      </div>
+    </div>
+  );
+};
 
 export default UserForm;
-
